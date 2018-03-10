d631a4c2926342a87b7fec11ab8cca0edbcef08e
Adam Lord <adam.lord@gmail.com>
2018-03-09 16:07:20 -0800

    Add ordering, cleanup and refactor

--- dtdrf/mixins.py
+++ dtdrf/mixins.py
@@ -1,11 +1,51 @@
+import re
 from collections import OrderedDict
 from rest_framework.response import Response
 
-import pprint
-pp = pprint.PrettyPrinter(indent=4)
-
 
 class DataTablesListMixin(object):
+
+    def assemble_dt_dict(self, request):
+        request_dict = request.GET
+        columns_dict = {
+            'columns': {},
+            'order': {}
+        }
+
+        for key in request_dict:
+            if 'columns' in key:
+                try:
+                    col_idx = re.match('columns\[(\d+)\]', key).group(1)
+                    if col_idx in columns_dict['columns']:
+                        continue
+                    else:
+                        col_prefix = 'columns[{}]'.format(col_idx)
+                        columns_dict['columns'][col_idx] = {
+                            'data': request_dict[col_prefix + '[data]'],
+                            'name': request_dict[col_prefix + '[name]'],
+                            'orderable': request_dict[col_prefix + '[orderable]'] == 'true',
+                            'search_regex': request_dict[col_prefix + '[search][regex]'] == 'true',
+                            'search_value': request_dict[col_prefix + '[search][value]'],
+                            'searchable': request_dict[col_prefix + '[searchable]'] == 'true',
+                        }
+                except AttributeError:
+                    continue
+            if 'order' in key:
+                try:
+                    order_idx = re.match('order\[(\d+)\]', key).group(1)
+                    if order_idx in columns_dict['order']:
+                        continue
+                    else:
+                        columns_dict['order'][order_idx] = {
+                            'column': request_dict['order[' + order_idx + '][column]'],
+                            'dir': request_dict['order[' + order_idx + '][dir]']
+                        }
+                except AttributeError:
+                    continue
+            else:
+                continue
+
+        return columns_dict
 
     def get_paginated_response(self, data):
         return Response(OrderedDict([
@@ -13,6 +53,7 @@ class DataTablesListMixin(object):
         ]))
 
     def list(self, request, *args, **kwargs):
+
         queryset = self.get_queryset()
         self.queryset_count = queryset.count()
 
@@ -31,9 +72,9 @@ class DataTablesListMixin(object):
         return response
 
     def dispatch(self, request, *args, **kwargs):
-        print()
-        pp.pprint(request.GET)
-        print()
+
+        self.dt_dict = self.assemble_dt_dict(request)
+
         response = super(DataTablesListMixin, self).dispatch(request, args, kwargs)
 
         draw = int(request.GET.get('draw', 1))
